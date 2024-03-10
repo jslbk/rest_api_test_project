da3652e537a4eb74ac0dc6073492b880f6382275
Julyan SLABKO <115033204+jslbk@users.noreply.github.com>
2024-03-11 00:27:01 +0200

    owner.Config improvement

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
+        "system:properties",
         "classpath:${env}.properties",
 })
 public interface WebDriverConfig extends Config {
